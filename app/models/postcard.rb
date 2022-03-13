9873a2cb40e5557484226835dd9497c54210b1be
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-03-13 21:56:14 +0000

    Direct association between users and postcards

--- app/models/postcard.rb
+++ app/models/postcard.rb
@@ -1,6 +1,9 @@
 class Postcard < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :postcards,
+             :dependent => :destroy
+
   has_many   :bookmarks,
              :dependent => :destroy
 
