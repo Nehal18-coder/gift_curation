84f628f56661f0179f01bb6bc530fb34ad4aea00
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-03-13 21:57:35 +0000

    Remove controller comments

--- app/controllers/postcards_controller.rb
+++ app/controllers/postcards_controller.rb
@@ -1,25 +1,20 @@
 class PostcardsController < ApplicationController
   before_action :set_postcard, only: %i[show edit update destroy]
 
-  # GET /postcards
   def index
     @q = Postcard.ransack(params[:q])
     @postcards = @q.result(distinct: true).includes(:user,
                                                     :category).page(params[:page]).per(10)
   end
 
-  # GET /postcards/1
   def show; end
 
-  # GET /postcards/new
   def new
     @postcard = Postcard.new
   end
 
-  # GET /postcards/1/edit
   def edit; end
 
-  # POST /postcards
   def create
     @postcard = Postcard.new(postcard_params)
 
@@ -35,7 +30,6 @@ class PostcardsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /postcards/1
   def update
     if @postcard.update(postcard_params)
       redirect_to @postcard, notice: "Postcard was successfully updated."
@@ -44,7 +38,6 @@ class PostcardsController < ApplicationController
     end
   end
 
-  # DELETE /postcards/1
   def destroy
     @postcard.destroy
     message = "Postcard was successfully deleted."
@@ -57,12 +50,10 @@ class PostcardsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_postcard
     @postcard = Postcard.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def postcard_params
     params.require(:postcard).permit(:postcard_image, :postcard_name,
                                      :description, :price, :delivery_time, :category_id, :bookmark_id, :user_id)
