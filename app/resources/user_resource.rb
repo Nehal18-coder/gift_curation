d7f0e889c86d050be04210af68e92dd29eb95f3c
anonymous anonymous <nehal.garg@kellogg.northwestern.edu>
2022-03-13 21:56:54 +0000

    Generate graphiti resources for users

---
+++ app/resources/user_resource.rb
@@ -0,0 +1,17 @@
+class UserResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :username, :string
+  attribute :password, :string
+  attribute :birthday, :date
+  attribute :interests, :string
+  attribute :address, :string
+  attribute :category_id, :integer
+  attribute :postcard_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
